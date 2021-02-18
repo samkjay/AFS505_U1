5e730e14aa69d05924d8421ee68618d9ba537ee9
SAM <samodya.jayasinghe@wsu.edu>
2021-02-18 11:52:48 -0800

    project

--- project/game_of_life.py
+++ project/game_of_life.py
@@ -2,10 +2,13 @@
 
 .. module:: Class projectU1
     :platform: Windows
-    :synopsis: This program would enable to run the "Game of life" with user specified inital conditions and number
+    :synopsis: This program would enable to run the "Game of life" with user specified initial conditions and number
     of iteration. The grid size has being defined by the programmer to be 30 x 80.
+    reviewer - Natalie Sanchez
+    reviewer grade -
 
 .. moduleauthor:: Sam Jayasinghe (samodyajayasinghe@wsu.edu)
+
 """
 
 
@@ -18,7 +21,7 @@ def count_neighbors(input_array, neighbor_sum, num_row, num_col):
      :param input_array: a 2D list(array) defined within the main function
      :type input_array:  a 2D list containing 0 or 1 values
 
-     :param neighbor_sum: a 2D list(array) storing the output of the function,the number of 'on(1)' neighbors for each cell in input array
+     :param neighbor_sum: a 2D list(array) storing the output of the function, the number of 'on(1)' neighbors for each cell in input array
      :type neighbor_sum:  a 2D list containing integers
 
      :param num_row: number of rows in input_array, the value is defined within the main function
@@ -26,6 +29,10 @@ def count_neighbors(input_array, neighbor_sum, num_row, num_col):
 
      :param col_row: number of columns in input_array, the value is defined within the main function
      :type col_row: integer
+
+     :return: returns the count of total "on" neighbors for each cell,  and saves it in
+     the 2D list neighbor_sum
+     :rtype: integers ranging from 0 to 8
 
      """
     for i in range(num_col):
@@ -36,36 +43,43 @@ def count_neighbors(input_array, neighbor_sum, num_row, num_col):
                 else:
                    #This line in each if statements adds '1' or '0' to the neighbor cell count of the tested cell, based on if it's 'on' or 'off'
                    neighbor_sum[j][i] += input_array[j-1][i]
+
                 # to check the neighbor square to south
                 if j == (num_row-1): #because this row does not have neighbors to the south, this line skips counting it
                    continue
                 else:
                    neighbor_sum[j][i] += input_array[j+1][i]
+
                 # to check the neighbor square to west
                 if  i== 0: #because this row does not have neighbors to the west, this line skips counting it
                    continue
                 else:
                    neighbor_sum[j][i] += input_array[j][i-1]
+
                 # to check the neighbor square to east
                 if i == (num_col-1): #because this row does not have neighbors to the east, this line skips counting it
                    continue
                 else:
                    neighbor_sum[j][i] += input_array[j][i+1]
+
                 # to check the neighbor square to north east
                 if j == 0 or i == (num_col-1): #because this row and column does not have neighbors to the north east, this line skips counting it
                    continue
                 else:
                    neighbor_sum[j][i] += input_array[j-1][i+1]
+
                 # to check the neighbor square to south east
                 if j == (num_row-1) or i == (num_col-1): #because this row and column does not have neighbors to the south east, this line skips counting it
                    continue
                 else:
                    neighbor_sum[j][i] += input_array[j+1][i+1]
+
                 # to check the neighbor square to north west
                 if j == 0 or i == 0: #because this row and column does not have neighbors to the north west,this line skips counting it
                    continue
                 else:
                    neighbor_sum[j][i] += input_array[j-1][i-1]
+
                 # to check the neighbor square to south west
                 if j ==  (num_row-1) or i == 0: #because this row and column does not have neighbors to the south west, this line skips counting it
                    continue
@@ -79,7 +93,7 @@ def apply_rules(input_array, neighbor_sum, num_row, num_col):
     :param input_array: a 2D list(array) defined within the main function
     :type input_array:  a 2D list containing 0 or 1 values
 
-    :param neighbor_sum: a 2D list(array) containg the total count of "on" neighbor cells for each cell in the input_array
+    :param neighbor_sum: a 2D list(array) containing the total count of "on" neighbor cells for each cell in the input_array
     :type neighbor_sum:  a 2D list containing integers
 
     :param num_row: number of rows in input_array, the value is defined within the main function
@@ -87,6 +101,10 @@ def apply_rules(input_array, neighbor_sum, num_row, num_col):
 
     :param col_row: number of columns in input_array, the value is defined within the main function
     :type col_row: integer
+
+    :return: returns a value of 1 or 0 to each cell in the input_array based on the defined rules.
+    the rules are based on the integers in each cells of the neighbor_sum array
+    :rtype: integer, 1 or 0
 
     """
 
@@ -120,13 +138,17 @@ def print_grid(input_array, num_row, num_col):
     """This is a function to print the 2D list(array) after each iteration
 
      :param input_array: a 2D list(array) defined within the main function
-     :type input_array:  a 2D list containing 0 or 1 values
+     :type input_array:  integer
 
      :param num_row: number of rows in input_array, the value is defined within the main function
      :type num_row: integer
 
      :param col_row: number of columns in input_array, the value is defined within the main function
      :type col_row: integer
+
+     :return: prints out a 2D list with num_col columns and num)row rows. each cell of the input_array
+      containing a value 1 would print a 'X' and every cell containing a value 0 would print a '-'
+     :rtype: a 2D list with 'X's and '-'s
 
      """
     #an array is defined to convert '1' and '0' values in the input_array to 'x' and '-'
@@ -150,11 +172,17 @@ def print_grid(input_array, num_row, num_col):
 def main():
     """This is the main executable function of this script
 
-     this function reads in the initial conditions specifies by the user for the game of life.
-     splits the "coordinates " given by the user to read the column and row numbers seperately.
-     the input_array is defined within this function as a 2D list(array)
+     This function reads in the initial conditions specifies by the user for the game of life.
+     The user specifies the number of iterations for each called-in functions.
+     This function also splits the "coordinates " given by the user to read the initially input column and row numbers separately.
+     The input_array, neighbor_sum array are defined as 2D lists(array)
+     The number of rows and columns in each of those arrays are defined
+     within this function. 
 
-     """
+     :return: returns a 2D list with 'X's and '-'
+     :rtype: a 2D list
+
+    """
     #a string to count the number of all arguments that is given by the user. a specific number of arguments are not unpacked using argv.
     all_args = len(argv)
     #the script is the first element in argv
@@ -171,6 +199,7 @@ def main():
 
     # initialize the array(2D list)
     for i in initial:
+
         #split the given initial coordinate (separated by a ':') and save row and column coordinates separately
         row_coordinate = int(i.split(":")[0])
         column_coordinate = int(i.split(":")[1])
@@ -198,4 +227,5 @@ def main():
          print_grid(arr, rows, cols)
 
 #calling in main function
-main()
+if __name__ == "__main__" :
+       main()
